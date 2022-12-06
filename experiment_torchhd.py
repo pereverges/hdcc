8940140ab66668d1c53ced502c87362cc311e2f0
pereverges <pere.verges@estudiantat.upc.edu>
2022-12-06 15:12:10 -0800

    experiment

--- experiment_torchhd.py
+++ experiment_torchhd.py
@@ -2,16 +2,15 @@ import os
 import sys
 import subprocess
 
-dimensions = [64]
-#, 128, 512, 1024, 4096, 10240]
+dimensions = [64, 128, 512, 1024, 4096, 10240]
 files = ['mnist', 'voicehd']
-repetitions = 1
+repetitions = 5
 
 for file in files:
-    with open("output.txt", "a") as output:
+    with open("output_torchhd.txt", "a") as output:
         output.write('\n' + file + '\n')
     for i in range(repetitions):
         for dim in dimensions:
             with open("output.txt", "a") as output:
-                res = subprocess.check_output(['python3', file + '.py', str(dim)])
+                res = subprocess.check_output(['python3', file + '.py', str(dim)]).decode(sys.stdout.encoding)
                 output.writelines(res)
