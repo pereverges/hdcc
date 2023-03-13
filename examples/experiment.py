40e0abf166cb6658a8b658f7ab6395c287ac35fe
pereverges <pere.verges@bsc.es>
2023-03-13 09:12:17 -0700

    Expermitent for ngram

--- examples/experiment.py
+++ examples/experiment.py
@@ -14,14 +14,19 @@ out_file = sys.argv[1]
 
 #dimensions = [64, 128, 512, 1024, 4096, 10240]
 dimensions = [32]
-files = ['languages','emgp','emgpp','emgppp','emgpppp','emgppppp','mnist', 'voicehd']
-train_size = [210032, 368, 345, 338, 333, 235, 60000, 6238]
-test_size = [21000, 158, 148, 145, 143, 101, 10000, 1559]
+files = ['languages','emgp','emgpp','emgppp','emgpppp','emgppppp']
+#,'mnist', 'voicehd']
+train_size = [210032, 368, 345, 338, 333, 235]
+#, 60000, 6238]
+test_size = [21000, 158, 148, 145, 143, 101]
+# 10000, 1559]
 vector_size = 128
 num_threads = 4
-type_exec = ['PARALLEL','SEQUENTIAL']
+type_exec = ['PARALLEL']
+#,'SEQUENTIAL']
 repetitions = 1
-vectorial = ['FALSE','TRUE']
+vectorial = ['TRUE']
+#,'FALSE']
 
 position = 58
 ti = '-v'
